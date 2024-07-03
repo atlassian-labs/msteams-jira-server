871ef42d807b837cd361da13b33a3bca609539bc
Zakhar Listiev <zlistiev@atlassian.com>
2024-07-03 13:28:23 +0300

    JSMT-170 updated libraries to fix VULN

---
+++ src/MicrosoftTeamsIntegration.Artifacts/Bots/Middleware/SetLocaleMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace MicrosoftTeamsIntegration.Artifacts.Bots.Middleware
+{
+    public class SetLocaleMiddleware : IMiddleware
+    {
+        private readonly string _defaultLocale;
+
+        public SetLocaleMiddleware(string defaultLocale)
+        {
+            _defaultLocale = defaultLocale ?? throw new ArgumentNullException(nameof(defaultLocale));
+        }
+
+        public async Task OnTurnAsync(ITurnContext context, NextDelegate next, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var cultureInfo = !string.IsNullOrWhiteSpace(context.Activity.Locale) ? new CultureInfo(context.Activity.Locale) : new CultureInfo(this._defaultLocale);
+
+            CultureInfo.CurrentUICulture = CultureInfo.CurrentCulture = cultureInfo;
+
+            await next(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
