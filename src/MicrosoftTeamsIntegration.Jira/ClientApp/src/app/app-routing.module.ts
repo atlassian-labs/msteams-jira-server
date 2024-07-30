fb0295593f2dfe7044b11f63efef3c4f3b019736
Volodymyr Batrukh <vbatrukh@atlassian.com>
2024-07-30 12:57:04 +0300

    Update to v15

--- src/MicrosoftTeamsIntegration.Jira/ClientApp/src/app/app-routing.module.ts
+++ src/MicrosoftTeamsIntegration.Jira/ClientApp/src/app/app-routing.module.ts
@@ -88,7 +88,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
     exports: [RouterModule]
 })
 export class RoutingModule { }
