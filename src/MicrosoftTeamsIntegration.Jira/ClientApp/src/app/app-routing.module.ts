f5ef562b2f19af669e839b51848b66abab30876a
Volodymyr Batrukh <vbatrukh@atlassian.com>
2024-07-30 12:17:44 +0300

    Step 7: Update to Angular 11

--- src/MicrosoftTeamsIntegration.Jira/ClientApp/src/app/app-routing.module.ts
+++ src/MicrosoftTeamsIntegration.Jira/ClientApp/src/app/app-routing.module.ts
@@ -88,7 +88,7 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    imports: [RouterModule.forRoot(routes, { useHash: true, relativeLinkResolution: 'legacy' })],
     exports: [RouterModule]
 })
 export class RoutingModule { }
